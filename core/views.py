fbfc2a7a509b5208e344fcdd4619b19f660a1ba6
Your Name <rsagadatov@gmail.com>
2021-08-21 22:09:08 +0500

    Добавлено использование ListMixin в MovieListView

--- core/views.py
+++ core/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
-from core.models import Movie, Director
+from core.models import Movie, Director, Actor
+
+
+class ListMixin:
+
+    def get_queryset(self):
+        queryset = self.model.objects.all()
+        print(list(queryset))
+        name = self.request.GET.get('name')
+        if name:
+            queryset = queryset.filter(name__istartswith=name)
+        print(name)
+        return queryset
 
 
 class HomeView(TemplateView):
     template_name = 'core/index.html'
 
 
-class MovieListView(ListView):
+class MovieListView(ListMixin, ListView):
     model = Movie
     template_name = 'core/movies.html'
 
@@ -20,3 +32,8 @@ class MovieDetailView(DetailView):
 class DirectorDetailView(DetailView):
     model = Director
     template_name = 'core/director_detail.html'
+
+
+class ActorListView(ListView):
+    model = Actor
+    template_name = 'core/actors.html'
