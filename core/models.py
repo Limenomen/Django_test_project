fca01356b58ef5c293c295ce23e3bb02217da26d
Your Name <rsagadatov@gmail.com>
2021-08-22 22:35:33 +0500

    добавлены поля для изображений в моделях

--- core/models.py
+++ core/models.py
@@ -15,7 +15,8 @@ class Director(models.Model):
     date_of_birth = models.DateField('дата рождения', null=True, blank=True)
     date_of_death = models.DateField('дата смерти', null=True, blank=True)
     country = models.CharField('страна', max_length=32, null=True, blank=True)
-    biography = models.CharField('биография', max_length=4096, null=True, blank=True)
+    biography = models.TextField('биография', max_length=4096, null=True, blank=True)
+    image = models.ImageField('Изображение', upload_to='media/directors/', blank=True)
 
     def __str__(self):
         return f"{self.first_name}, {self.last_name}"
@@ -24,8 +25,9 @@ class Director(models.Model):
 class Movie(models.Model):
     name = models.CharField('Название фильма', max_length=64)
     director = models.ForeignKey('Director', on_delete=models.SET_NULL, null=True)
-    description = models.CharField('описание', max_length=4096)
+    description = models.TextField('описание', max_length=4096)
     genre = models.ManyToManyField(Genre)
+    image = models.ImageField('Изображение', upload_to='media/movies/', blank=True)
 
     YEAR_CHOICES = [(year, year) for year in range(1950, (datetime.now().year + 5))]
     release_date = models.IntegerField('год выхода', choices=YEAR_CHOICES, default=2000, blank=True)
