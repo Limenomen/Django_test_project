51436fdd799e0ae3a5e31a6ae25dc2ae93c2054c
Your Name <rsagadatov@gmail.com>
2021-08-23 18:30:15 +0500

    отображение моделей в admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 import core.models as models
 
+
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+class MoviesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'release_date', 'director')
+
+
 admin.site.register(models.Genre)
-admin.site.register(models.Movie)
-admin.site.register(models.Director)
+admin.site.register(models.Movie, MoviesAdmin)
+admin.site.register(models.Director, DirectorAdmin)
